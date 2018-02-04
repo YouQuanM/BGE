617c406c933cd9273c664c2b52c5f0b43ca92f2b
YouQuanM <31495434+YouQuanM@users.noreply.github.com>
2018-02-04 23:07:29 +0800

    加了一些注释啥的
    做了一些微小的工作

--- BGEbianpai.js
+++ BGEbianpai.js
@@ -1,5 +1,6 @@
 //贝格尔编排法
 var arr = [];
+//点击添加队伍：检测队伍名称是否为空，检测队伍名称是否已经存在
 function Addteam() {
     var name = $(" #team-name ").val();
     if (name === ''){
@@ -14,8 +15,10 @@ function Addteam() {
             alert("已经输入过该队伍了！")
         }
     }
+    //输入完成后清空input输入框，并放置焦点
     $("input[id='team-name']").val("").focus();
 }
+//队伍分组排列：队伍数为单数增加轮空，4支以下队伍无法排序
 function BGE() {
     if (arr.length % 2 !== 0){
         arr.push("轮空");
@@ -23,10 +26,14 @@ function BGE() {
     if(arr.length > 4){
         var step = (arr.length+arr.length%2-4)/2;
         for(let i = 1; i < arr.length; i++){
+            //输出共多少轮次的比赛为<th>
             $("#teamBuild-head").append("<th>"+"第"+i+"轮"+"</th>");
+            //输出每一轮次的比赛为<td>，并为每个td加id
             $("#teamBuild-body").append("<td id="+i+">");
             for(var j = 0; j < arr.length/2; j++){
+                //输出每一组的对战情况
                 var text_out = (arr[j]+"-"+arr[arr.length-j-1]);
+                //在consloe里也输出一个
                 console.log(arr[j]+"-"+arr[arr.length-j-1]);
                 $("td[id="+i+"]").append(text_out + "<br>");
             }
@@ -49,14 +56,10 @@ function BGE() {
         alert("需要四只队伍以上");
     }
 }
+//清空所有数据
 function Clear() {
     arr.length = 0;
     $("#teamBuild-body").empty();
     $("#teamBuild-head").empty();
     $("#teamName").empty();
 }
-$("button[id=Add]").on("keydown",function(e){
-    if(e.key==="Enter"){
-        $("#btnSubmit").trigger("click");
-    }
-})
